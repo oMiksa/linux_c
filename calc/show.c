029f5792d02588bf9c518c27ac64e911752c1952
Miksa <zis.is.kiss@gmail.com>
2018-09-21 23:52:20 +0700

    fix_calcul+.so

--- calc/show.c
+++ calc/show.c
@@ -1,61 +1,70 @@
 #include <stdio.h>
+#include "show.h"
 #include "libcalc.h"
 
-void show()
-{
-    int first, second, result;
-    int index;
-    char key;
-    int run = 0;
-    while(!run){
-        printf("1)add\n");
-        printf("2)ded\n");
-        printf("3)mult\n");
-        printf("4)div\n");
-        printf("5)exit\n");
-        printf("?.");
+void show(){
+    short index;
+    short run = INPUT;
+    while(run){
+        if(run == INPUT){
+            print_menu();
 
-        scanf("%c", &key);
-        index = (int)key;
-
-        switch (index - 48)
-        {
-            case 1:  
-                printf("in first number ");
-                scanf("%d", &first);
-                printf("in second number ");
-                scanf("%d", &second);
-                printf("result = %d\n\n",add(first,second));
-                break;
-            case 2: 
-                printf("in first number ");
-                scanf("%d", &first);
-                printf("in second number ");
-                scanf("%d", &second);
-                printf("result = %d\n\n",ded(first, second));
-                break;
-            case 3: 
-                printf("in first number ");
-                scanf("%d", &first);
-                printf("in second number ");
-                scanf("%d", &second);
-                printf("result = %d\n\n",mult(first, second));
-                break;
-            case 4: 
-                printf("in first number ");
-                scanf("%d", &first);
-                printf("in second number ");
-                scanf("%d", &second);
-                printf("result = %d\n\n",div(first, second));
-                break;
-            case 5:
-                printf("bye\n"); 
-                run++; 
-                break;
-            default: 
-                printf("wrong choice\n\n"); 
-                break;
+            index = cmd();
+            if(index) run = OUTPUT;
         }
-        index = 0;
+
+        if(run == OUTPUT){
+            switch(index){
+                case 1:  
+                    operation(add);
+                    run = INPUT;
+                    break;
+                case 2: 
+                    operation(ded);
+                    run = INPUT;
+                    break;
+                case 3: 
+                    operation(mult);
+                    run = INPUT;
+                    break;
+                case 4: 
+                    operation(div);
+                    run = INPUT;
+                    break;
+                case 5:
+                    printf("bye\n"); 
+                    run = EXIT; 
+                    break;
+                default: 
+                    printf("wrong choice\n\n"); 
+                    break;
+            }
+        }
     }
+}
+
+void print_menu(){
+    printf("1)add\n");
+    printf("2)ded\n");
+    printf("3)mult\n");
+    printf("4)div\n");
+    printf("5)exit\n");
+    printf("?.");
+}
+
+short cmd(){
+    short cmd;
+    scanf("%d", &cmd);
+    if((cmd > 0)&&(cmd < 6))
+        return cmd;
+    else return 0;    
+}
+
+void operation(int (*fn)(int, int)){
+    int first, second;
+    printf("in first number ");
+    scanf("%d", &first);
+    printf("in second number ");
+    scanf("%d", &second);
+    printf("result = %d\n\n",fn(first, second));
 }
